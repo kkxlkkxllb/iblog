c46ab08d0cc183886a142449715b85cefc3b1892
bill lee <oncwnuJR2MppHKCmEaX54FYRt-XU@git.weixin.qq.com>
2019-12-20 11:33:01 +0800

    fix

--- precache-manifest.5564823eab82be20e244ab520860cc6c.js
+++ precache-manifest.5564823eab82be20e244ab520860cc6c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "2aaae5b9f1161bd57307",
+    "revision": "591ec9d943285fe272d1",
     "url": "/css/app.34e0e13b.css"
   },
   {
-    "revision": "2aaae5b9f1161bd57307",
-    "url": "/js/app.db40fc04.js"
+    "revision": "591ec9d943285fe272d1",
+    "url": "/js/app.f0442f9b.js"
   },
   {
     "revision": "c5fe7f1f3ebc4ea0e135",
@@ -20,10 +20,6 @@ self.__precacheManifest = [
     "url": "/js/pdfjsWorker.287d4441.js"
   },
   {
-    "revision": "3db19eb9bdd243768562513ddf1684d1",
-    "url": "/fonts/icomoon.3db19eb9.eot"
-  },
-  {
     "revision": "aa54e842e8a9474227a6a7c517aac5f2",
     "url": "/fonts/icomoon.aa54e842.ttf"
   },
@@ -32,16 +28,16 @@ self.__precacheManifest = [
     "url": "/fonts/icomoon.e4359b18.woff"
   },
   {
+    "revision": "3db19eb9bdd243768562513ddf1684d1",
+    "url": "/fonts/icomoon.3db19eb9.eot"
+  },
+  {
     "revision": "d3bf4e0f0069d509611e57cabf202c99",
     "url": "/img/icomoon.d3bf4e0f.svg"
   },
   {
-    "revision": "499cab7e90af403f50cfaff20439e500",
+    "revision": "5751d9395b0e03983ac3a2dae1698525",
     "url": "/index.html"
-  },
-  {
-    "revision": "11a4cf9465a2c86ff00fdeb5cf88244e",
-    "url": "/favicon.png"
   },
   {
     "revision": "e496ee3d74d3a3f99cd7f195a2ec5529",
@@ -50,6 +46,10 @@ self.__precacheManifest = [
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/robots.txt"
+  },
+  {
+    "revision": "11a4cf9465a2c86ff00fdeb5cf88244e",
+    "url": "/favicon.png"
   },
   {
     "revision": "e0b7d92db64f99aca86b51ab68ca874c",
